30f9d0a13d67d8c39ebfa1ea66397f8d6de3f39a
Derek Ryter <dryter@usgs.gov>
2022-02-23 17:57:57 -0700

    files and scripts

--- script/bias_correct.py
+++ script/bias_correct.py
@@ -1,3 +1,20 @@
+def getmonth(df, fld, param):
+    df['mo'] = pd.DatetimeIndex(df['date']).month
+    ar_mm = np.array([],dtype=float)
+    startdate = df['date'].min()
+    enddate = df['date'].max()
+    nyr = enddate.year - startdate.year
+    molist = [10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for m in molist:
+        df_m = df.loc[df['mo']==m]
+        if param=='ppt':
+            ar_mm = np.append(ar_mm, df_m[fld].sum())
+        else:
+            ar_mm = np.append(ar_mm, df_m[fld].mean())
+    if param=='ppt':
+        ar_mm = ar_mm/nyr
+    return ar_mm
+
 def getannual(df, fld):
     startdate = df['date'].min()
     startyear = startdate.year
@@ -37,13 +54,14 @@ df_mod_hist['date'] = df_mod_hist['date'].astype('datetime64')
 # read in observed data
 df_obs = pd.read_csv('data_file_2014.csv', header=0)
 df_obs['date'] = pd.to_datetime(df_obs[['year', 'month', 'day']])
+obs_month = getmonth(df_obs, 'ppt', 'ppt')
 # merge with the model data
 df_mod_obs = df_obs.merge(df_mod_hist, how='inner', left_on='date', right_on='date')
 # get the date range of the common data
 startdate = df_mod_obs['date'].min()
 enddate = df_mod_obs['date'].max()
 ##########################################
-paramindex = 1
+paramindex = 2
 ##########################################
 # pull out the observed data
 obs_data = df_mod_obs[paramlist[paramindex]].values
@@ -62,7 +80,7 @@ for modindex in range(4):
     # pull out the model historical data
     model_data = df_mod_obs[fld].values
     model_data_s = model_data[:, np.newaxis, np.newaxis]
-
+    mod_month = getmonth(df_mod_hist, fld, paramlist[paramindex])
     fld = '{}_{}_{}'.format(modlist[modindex], scenlist[0], paramlist[paramindex])
     sce_data = df_sce[fld].values
     sce_data_s = sce_data[:, np.newaxis, np.newaxis]
@@ -87,22 +105,26 @@ for modindex in range(4):
         df2 = bc.correct(method='basic_quantile')
         #df2 = bc.correct(method='modified_quantile')
     correct_values = df2.values[0][0]
+
     #df_sce_out[fld] = correct_values
     df = pd.DataFrame()
     df['date'] = df_mod_obs['date']
     df[fld] = model_data
     df[fld + '_cor'] = correct_values
+    cor_month = getmonth(df, fld + '_cor', paramlist[paramindex])
     df[paramlist[paramindex]] = df_mod_obs[paramlist[paramindex]]
     ar_yr, ar_uncor = getannual(df, fld)
     ar_yr, ar_corr = getannual(df, fld + '_cor')
     ar_yr, ar_obs = getannual(df, paramlist[paramindex])
 
+    molist = [10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    monames = ['Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
     fig, ax = plt.subplots(figsize=(10, 7), tight_layout=True)
-    ax.plot(df_mod_obs['date'], np.cumsum(model_data), 'b-', lw=0.5, label='uncorrected')
+    ax.plot(monames, mod_month, 'b-', lw=0.5, label='model uncorrected')
+    ax.plot(monames, cor_month, 'g-', lw=0.5, label='model corrected')
+    ax.plot(monames, obs_month, color='skyblue', lw=0.5, label='observed')
     #plt.axhline(np.mean(ar_uncor), color='blue', lw = 0.5, ls='--', label='mean uncorrected = {:2.1f}'.format(np.mean(ar_uncor)))
-    ax.plot(df_mod_obs['date'], np.cumsum(correct_values), 'g-', lw=0.5, label='corrected')
-    #plt.axhline(np.mean(ar_corr), color='green', lw = 0.5, ls='--', label='mean corrected = {:2.1f}'.format(np.mean(ar_corr)))
-    ax.plot(df_mod_obs['date'], np.cumsum(obs_data), color='skyblue', lw=0.5, label='observed')
+#plt.axhline(np.mean(ar_corr), color='green', lw = 0.5, ls='--', label='mean corrected = {:2.1f}'.format(np.mean(ar_corr)))
     #plt.axhline(np.mean(ar_obs), color='skyblue', lw = 0.5, ls='--', label='mean observed = {:2.1f}'.format(np.mean(ar_obs)))
     #diff = ar_uncor-ar_corr
     #ax.plot(ar_yr, ar_uncor-ar_corr, 'g--', lw=0.5, label='uncorrected - corrected')
